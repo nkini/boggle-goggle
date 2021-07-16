9cab11f136f76e8a9bd39bfa4a0eb0756431a5ca
Nikhil Kini <nkini@ucsc.edu>
2021-07-16 00:54:08 -0400

    Solve specific game via web interface
    Also changed the endpoints:
    /      - select between random and specific game
    /play  - play a randomly generated board
    /solve - enter specific game and see solution

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from board import Board
+from util import get_board_input
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
+    return render_template("index.html")
+
+@app.route("/play")
+def play():
     BOARD = Board(board_size=4)
     BOARD.solve(listname="twl06")
-    return render_template("index.html", board=BOARD)
+    return render_template("board.html", board=BOARD)
+
+@app.route("/solve")
+def solve():
+    if 'board' in request.args:
+        board_input = get_board_input(request.args['board'])
+        BOARD = Board(board=board_input)
+        BOARD.solve(listname="twl06")
+        return render_template("board.html", board=BOARD)
+    else:
+        return render_template("board-input.html")
